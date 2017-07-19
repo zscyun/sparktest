d52a4998817abf1cde607913af9dab2cb8499a3b
zscyun <zscyun@hotmail.com>
2017-07-20 01:48:25 +0800

    complete

--- src/main/scala/com/nsn/npo/mydo/research/spark/wordcount/App.scala
+++ src/main/scala/com/nsn/npo/mydo/research/spark/wordcount/App.scala
@@ -1,10 +1,11 @@
 package com.nsn.npo.mydo.research.spark.wordcount
 
+import org.apache.log4j.Level
+import org.apache.log4j.Level
+import org.apache.log4j.Logger
+import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
-import org.apache.spark.SparkConf
-import org.apache.log4j.Level
-import org.apache.log4j.{ Level, Logger }
 
 /**
  * @author ${user.name}
@@ -20,13 +21,18 @@ object App {
   Logger.getLogger("org").setLevel(Level.ERROR);
 
   def main(args: Array[String]) {
-    val logFile = "d:\\test.txt" // Should be some file on your system  
+     val javaObj = new MyJavaTest()
+    javaObj.sayHellow()
+    
+    val logFile = javaObj.testFilePath //"d://test.txt" // Should be some file on your system  
     val conf = new SparkConf().setAppName("Simple Application").setMaster("local[2]")
     val sc = new SparkContext(conf)
     val logData = sc.textFile(logFile, 2).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
     println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
+    
+   
   }
 
 }
